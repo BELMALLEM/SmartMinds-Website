d2100017f81e16eb57c88a1e749b66ba0f11f7a7
BELMALLEM Marouane <47851522+BELMALLEM@users.noreply.github.com>
2020-12-28 23:16:09 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => 'postgres://zamfwbnapexmgr:472e6f44859112f3b505c2e3271675d93476cb5e12b7e135cc5586534fed7da6@ec2-23-20-20-150.compute-1.amazonaws.com:5432/dea2gq399sr0qf',
+            'url' => ${{ secrets.DATABASE_URL }},
             'host' => 'ec2-23-20-20-150.compute-1.amazonaws.com',
             'port' => '5432',
             'database' => 'dea2gq399sr0qf',
